b6855c22a7ed978408fa1db05b1440fc3b69106c
Sune Simonsen <sune@we-knowhow.dk>
2020-09-14 21:47:35 +0200

    Publish example to gh-pages

--- example/src/routes.js
+++ example/src/routes.js
@@ -1,11 +1,11 @@
 import { Routes, Route } from "@nano-router/react";
 
 export const routes = new Routes(
-  new Route("posts/edit", "/posts/:postId"),
-  new Route("users/view", "/users/:userId"),
-  new Route("users/posts", "/users/:userId/posts"),
-  new Route("users/photos", "/users/:userId/photos"),
-  new Route("users/photos/view", "/users/:userId/photos/:photoId"),
-  new Route("users/todos", "/users/:userId/todos"),
-  new Route("users", "/")
+  new Route("posts/edit", "/nano-router/posts/:postId"),
+  new Route("users/view", "/nano-router/users/:userId"),
+  new Route("users/posts", "/nano-router/users/:userId/posts"),
+  new Route("users/photos", "/nano-router/users/:userId/photos"),
+  new Route("users/photos/view", "/nano-router/users/:userId/photos/:photoId"),
+  new Route("users/todos", "/nano-router/users/:userId/todos"),
+  new Route("users", "/nano-router")
 );
