ea7b49f9e9970174246bb819c0f1a65517778129
Sune Simonsen <sune@we-knowhow.dk>
2020-09-20 20:52:09 +0200

    Support Node >=10 without tooling

---
+++ packages/react/rollup.config.js
@@ -0,0 +1,7 @@
+export default {
+  input: "lib/index.js",
+  output: {
+    file: "dist/bundle.cjs.js",
+    format: "cjs",
+  },
+};
---
+++ packages/routes/rollup.config.js
@@ -0,0 +1,7 @@
+export default {
+  input: "src/index.js",
+  output: {
+    file: "dist/bundle.cjs.js",
+    format: "cjs",
+  },
+};
