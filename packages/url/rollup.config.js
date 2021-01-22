cd942f5731a21c12155d7c2a07a9a4f797500e6b
Sune Simonsen <sune@we-knowhow.dk>
2021-01-22 10:36:10 +0100

    Bundle the querystring module

--- packages/url/rollup.config.js
+++ packages/url/rollup.config.js
@@ -1,7 +1,27 @@
-export default {
-  input: "src/index.js",
-  output: {
-    file: "dist/bundle.cjs",
-    format: "cjs",
+import commonjs from "@rollup/plugin-commonjs";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+
+export default [
+  {
+    input: "src/index.js",
+    output: {
+      file: "dist/url.cjs",
+      format: "cjs",
+    },
+    external: ["querystring", "@nano-router/path"],
   },
-};
+  {
+    input: "src/index.js",
+    output: {
+      file: "dist/url.js",
+      format: "esm",
+    },
+    plugins: [
+      commonjs(),
+      nodeResolve({
+        preferBuiltins: false,
+      }),
+    ],
+    external: ["@nano-router/path"],
+  },
+];
